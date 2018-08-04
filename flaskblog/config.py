aacec58b506a0b869bb5b34257601cc2758655b1
Edward <ed_caf@protonmail.com>
2018-08-04 19:22:52 -0400

    Initial commit of flask blog

---
+++ flaskblog/config.py
@@ -0,0 +1,10 @@
+
+class Config:
+  SECRET_KEY = 'ba31dd4fdd1fce5f8fdd729ca67c0ff6'
+  SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+  MAIL_SERVER = 'smtp.gmail.com'
+  MAIL_PORT = 465
+  MAIL_USE_TLS = False
+  MAIL_USE_SSL = True
+  MAIL_USERNAME = 'ed22494@gmail.com'
+  MAIL_PASSWORD = 'Cafarella24?'
