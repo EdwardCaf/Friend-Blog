1853183238f782325b88751a32b5694d0cf511ba
Edward <ed_caf@protonmail.com>
2018-08-04 23:34:15 -0400

    Changed sidebar and navbar. Added larger content on create post

--- flaskblog/posts/forms.py
+++ flaskblog/posts/forms.py
@@ -4,6 +4,6 @@ from wtforms.validators import DataRequired
 
 class PostForm(FlaskForm):
   title = StringField('Title', validators=[DataRequired()])
-  content = TextAreaField('Content', validators=[DataRequired()])
+  content = TextAreaField('Content', validators=[DataRequired()], render_kw={"rows": 7, "cols": 11})
   submit = SubmitField('Post')
 
